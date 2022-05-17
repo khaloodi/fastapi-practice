4579f2892fe26fc04ff2ffcd618a633db7ac0043
Khaled Adad <khaledadad@lewisu.edu>
2022-05-17 00:22:33 -0500

    wip

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,6 +1,6 @@
-from typing import Optional
+from typing import Optional, List, Dict
 
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, Body, Path
 from pydantic import BaseModel
 
 router = APIRouter(
@@ -8,10 +8,18 @@ router = APIRouter(
     tags=['blog']
 )
 
+class Image(BaseModel):
+    url: str
+    alias: str
+
 class BlogModel(BaseModel):
     title: str
     content: str
-    date: Optional[bool]
+    nb_comments: str
+    published: Optional[bool]
+    tags: List[str] = []
+    metadata: Dict[str, str] = {'key1': 'val1'}
+    image: Optional[Image] = None #using our custom subtype
 
 @router.post('/new') #-> /blog/new ...
 def create_blog(blog: BlogModel):
@@ -25,15 +33,29 @@ def create_blog(blog: BlogModel, id: int, version: int=1):
         'version': version
     }
 
-@router.post('/new/{id}/comment')
+@router.post('/new/{id}/comment/{comment_id}')
 def create_comment(blog: BlogModel, id: int,
-                   comment_id:int = Query(None,
-                                          alias='commentId',
-                                          deprecated=True,
-                                          title='Id of the comment',
-                                          description='Some description for comment id')):
+                   comment_title: int = Query(None,
+                                              title='Id of the comment',
+                                              description='Some description for comment title',
+                                              alias='commentTitle',
+                                              deprecated=True,
+                                           ),
+                   # content: str = Body('hi how are you')
+                   # content: str = Body(...) # could also use Body(Ellipsis); this creates a required parameter
+                   content: str = Body(...,
+                                       min_length=10,
+                                       max_length=50,
+                                       regex='^[a-z\s]*$'
+                                       ),
+                   v: Optional[List[str]] = Query(['1.1','1.2','1.3']),
+                   comment_id: int = Path(None, gt=5, le=10)
+                   ):
     return {
         'blog': blog,
         'id': id,
-        'comment_id': comment_id,
+        'comment_title': comment_title,
+        'content': content,
+        'version': v,
+        'comment_id': comment_id
     }
